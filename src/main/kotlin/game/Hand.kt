c36e5ea0d4f653272da8525932ce271d57bad31f
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-07-11 13:52:39 +0200

    hasBlackjack tests

--- src/main/kotlin/game/Hand.kt
+++ src/main/kotlin/game/Hand.kt
@@ -4,9 +4,11 @@ import card.Card
 import card.Shoe
 import card.Face
 
-class Hand(private val cards: List<Card>, private val splitted: Boolean = false) {
+class Hand private constructor(private val cards: List<Card>, private val hasSplit: Boolean = false) : Comparable<Hand> {
     constructor(vararg cards: Card, splitted: Boolean = false) : this(cards.toList(), splitted)
     constructor(shoe: Shoe) : this(shoe.deal(), shoe.deal())
+
+    operator fun contains(card: Card) = cards.contains(card)
 
     fun total(): Int {
         val sum = cards.sumOf { it.rank.value() }
@@ -18,13 +20,33 @@ class Hand(private val cards: List<Card>, private val splitted: Boolean = false)
     }
 
     fun split(shoe: Shoe): List<Hand> {
-        require(!splitted && cards.isSplittable()) {
-            "Cannot split a hand which is not splittable"
+        if(hasSplit || !cards.isSplittable()) {
+            error("Cannot split a hand which is not splittable")
         }
-        return cards.chunked(1).map { Hand(it, splitted = true).hit(shoe) }
+        return cards.chunked(1).map { Hand(it, hasSplit = true).hit(shoe) }
     }
 
-    fun hit(shoe: Shoe) = Hand(cards + shoe.deal())
+    fun hit(shoe: Shoe) = Hand(cards + shoe.deal(), hasSplit = hasSplit)
+
+    private fun isBust() = total() > 21
+
+    /**
+     * Compares this object with the specified object for order. Returns zero if this object is equal
+     * to the specified [other] object, a negative number if it's less than [other], or a positive number
+     * if it's greater than [other].
+     */
+    override fun compareTo(other: Hand): Int {
+        if (isBust() && !other.isBust()) return -1
+        else if (!isBust() && other.isBust()) return 1
+        return this.total().compareTo(other.total())
+    }
+
+    override fun toString(): String {
+        return "Hand(cards=$cards)"
+    }
+
+    fun hasBlackjack() = !hasSplit && cards.size == 2 && total() == 21
+
 }
 
 private fun List<Card>.hasAce() = this.find { it.rank == Face.Ace } != null
--- tests/src/test/kotlin/game/HandTest.kt
+++ tests/src/test/kotlin/game/HandTest.kt
@@ -1,11 +1,14 @@
 package game
 
 import card.Face.Ace
+import card.Face.Queen
 import card.Shoe
 import card.Suit.*
 import card.of
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class HandTest {
@@ -84,5 +87,62 @@ class HandTest {
         val shoe = Shoe()
         val hand = Hand(shoe = shoe)
         assertTrue(hand.total() in 1..21) // Checks that cards have actually been dealt to the hand
+    }
+
+    @Test
+    fun `can split a hand`() {
+        val shoe = Shoe() + Shoe(cards = listOf(Ace of Spades, Ace of Diamonds))
+        val hand = Hand(shoe = shoe)
+        assertEquals(12, hand.total())
+        val (first, second) = hand.split(shoe)
+        assertTrue(Ace of Spades in first)
+        assertTrue(Ace of Diamonds in second)
+        assertThrows<IllegalStateException> {
+            first.split(shoe)
+        }
+        assertThrows<IllegalStateException> {
+            second.split(shoe)
+        }
+    }
+
+    @Test
+    fun `cannot split a hand that is already split, even if the card value are equal`() {
+        val shoe = Shoe() + Shoe(cards = listOf(Ace of Spades, Ace of Diamonds, Ace of Spades, Ace of Diamonds))
+        val hand = Hand(shoe = shoe)
+        val (first, second) = hand.split(shoe)
+        assertThrows<IllegalStateException> {
+            first.split(shoe)
+        }
+        assertThrows<IllegalStateException> {
+            second.split(shoe)
+        }
+    }
+
+    @Test
+    fun `can hit a hand`() {
+        val shoe = Shoe() + Shoe(cards = listOf(8 of Hearts, 2 of Spades, Ace of Spades))
+        var hand = Hand(shoe)
+        hand = hand.hit(shoe)
+        assertEquals(21, hand.total())
+        assertFalse(hand.hasBlackjack())
+    }
+
+    @Test
+    fun `does not have blackjack after split`() {
+        val shoe = Shoe() + Shoe(cards = listOf(Ace of Spades, Ace of Hearts, Queen of Diamonds, 10 of Diamonds))
+        val hand = Hand(shoe = shoe)
+        val (first, second) = hand.split(shoe)
+        assertEquals(21, first.total())
+        assertEquals(21, second.total())
+        assertFalse(first.hasBlackjack())
+        assertFalse(second.hasBlackjack())
+    }
+
+    @Test
+    fun `has blackjack`() {
+        val shoe = Shoe() + Shoe(cards = listOf(Ace of Spades, 10 of Diamonds))
+        val hand = Hand(shoe = shoe)
+        println(hand.total())
+        assertTrue(hand.hasBlackjack())
     }
 }
