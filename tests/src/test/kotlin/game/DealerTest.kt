a021d2677da5588d656ded8c13839b58fb3ef1ea
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-07-17 13:54:57 +0200

    Add dealertest

---
+++ tests/src/test/kotlin/game/DealerTest.kt
@@ -0,0 +1,72 @@
+package game
+
+import card.Face
+import card.Shoe
+import card.Suit
+import card.of
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DealerTest {
+    @Test
+    fun testTotal() {
+        val hand = BlackjackHand(10 of Suit.Hearts, Face.Ace of Suit.Spades)
+        val dealer = Dealer(hand)
+        assertEquals(21, dealer.total())
+    }
+
+    @Test
+    fun testActions() {
+        val hand = BlackjackHand(10 of Suit.Hearts, Face.Ace of Suit.Spades)
+        val dealer = Dealer(hand)
+        val actions = dealer.actions()
+        assertEquals(2, actions.size)
+        assertTrue(actions.contains(Action.Hit))
+        assertTrue(actions.contains(Action.Stand))
+    }
+
+    @Test
+    fun testPlayHit() {
+        val shoe = Shoe(cards = mutableListOf(2 of Suit.Hearts, 5 of Suit.Spades))
+        val hand = BlackjackHand(10 of Suit.Hearts, 7 of Suit.Spades)
+        val dealer = Dealer(hand)
+        dealer.play(Action.Hit, shoe)
+        assertEquals(19, dealer.total())
+    }
+
+    @Test
+    fun testPlayStand() {
+        val shoe = Shoe(cards = mutableListOf(2 of Suit.Hearts, 5 of Suit.Spades))
+        val hand = BlackjackHand(10 of Suit.Hearts, 7 of Suit.Spades)
+        val dealer = Dealer(hand)
+        dealer.play(Action.Stand, shoe)
+        assertEquals(17, dealer.total())
+    }
+
+    @Test
+    fun testHasBlackjack() {
+        val hand = BlackjackHand(10 of Suit.Hearts, Face.Ace of Suit.Spades)
+        val dealer = Dealer(hand)
+        assertTrue(dealer.hasBlackjack())
+    }
+
+    @Test
+    fun testIsFinished() {
+        val hand = BlackjackHand(10 of Suit.Hearts, 7 of Suit.Spades)
+        val dealer = Dealer(hand)
+        assertFalse(dealer.isFinished())
+        dealer.play(Action.Hit, Shoe())
+        assertTrue(dealer.isFinished())
+        assertTrue(dealer.actions().isEmpty())
+    }
+
+    @Test
+    fun testPrintCards() {
+        val shoe = Shoe(cards = mutableListOf(2 of Suit.Hearts, 5 of Suit.Spades))
+        val hand = BlackjackHand(10 of Suit.Hearts, 7 of Suit.Spades)
+        val dealer = Dealer(hand)
+        assertEquals("[10 of Hearts, <HIDDEN>]", dealer.printCards())
+        dealer.play(Action.Hit, shoe)
+        assertEquals("[10 of Hearts, 7 of Spades, 2 of Hearts]", dealer.printCards())
+    }
+}
